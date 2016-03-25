db61a3132418b5d14b9f4d03f0fb7abab20cfcb5
gnifeifeiing <645485165@qq.com>
2016-03-25 14:29:56 +0800

    记录

---
+++ Peoplehospital/app/src/main/java/com/zkrkj/peoplehospital/record/SeeDocDetail.java
@@ -0,0 +1,32 @@
+package com.zkrkj.peoplehospital.record;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zkrkj.peoplehospital.R;
+
+import base.BaseActivity;
+
+public class SeeDocDetail extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_doc_detail);
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initAction() {
+
+    }
+}
